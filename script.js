5ed5b4b523508ec3cc044a7bb3203051ed10318a
Jawed Sheikh <96526142+jawed26@users.noreply.github.com>
2025-12-28 14:59:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 $(document).ready(function () {
 
-    $('.testimonial-slider').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      arrows: false,
-      dots: true,
-      autoplay: false,
-      infinite: true,
-      responsive: [
-        {
-          breakpoint: 999,
-          settings: {
-            slidesToShow: 2
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
-
     $('body').on('click','.nav-menu-bar', function () {
       $(this).addClass('active');
       $(this).closest('.header-flex').find('.navbar').addClass('active');
